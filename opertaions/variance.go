df6d0c24e26fdbe6807c6182e7127e78998547e9
achent <chentaymane234@gmail.com>
2025-12-11 15:57:57 +0100

    ff

--- opertaions/variance.go
+++ opertaions/variance.go
@@ -7,13 +7,7 @@ func Variance(numbers []int, Average int) int {
 
 	for _, r := range numbers {
 		nmbr := r - Average
-		if nmbr < 0 {
-			nmbr = -nmbr
-			variance += float64(nmbr * nmbr)
-		} else {
-			variance += float64(nmbr * nmbr)
-		}
-
+		variance += float64(nmbr * nmbr)
 	}
 	variance = variance / float64(len(numbers))
 	return int(math.Round(variance))
